1475c0b3fbda13a5372a2703db5fcd200c782d67
DanielFaour <daniea1602@gmail.com>
2026-05-21 19:04:11 +0200

    update info

--- riggr/src/components/Footer.jsx
+++ riggr/src/components/Footer.jsx
@@ -7,8 +7,9 @@ function Footer() {
           <p>Lei lyd og utstyr enkelt i Oslo.</p>
         </div>
         <div className="footer-contact">
-          <a href="#informasjon">Personvern og leievilkår</a>
-          <a href="mailto:kontakt@riggr.no">kontakt@riggr.no</a>
+          <p>Kontakt:</p>
+          {/* <a href="#informasjon">Personvern og leievilkår</a> */}
+          <a href="mailto:daniea1602@gmail.com">daniea1602@gmail.com</a>
           <span>Oslo, Norge</span>
         </div>
       </div>
